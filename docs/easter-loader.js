656c9425ab0406176821633acc801443dcf283a1
Vegard Økland <vegard.okland@schibsted.com>
2019-12-20 09:13:54 +0100

    Track output

---
+++ docs/easter-loader.js
@@ -0,0 +1,35 @@
+(function loader() {
+  function load() {
+    const scriptEl = document.createElement('script');
+    scriptEl.setAttribute('src', 'snake.js');
+    scriptEl.setAttribute('type', 'application/javascript');
+    document.getElementsByTagName('body').item(0).append(scriptEl);
+
+    document.removeEventListener('keyup', keyhandler);
+  }
+
+  const code = [
+    'arrowup',
+    'arrowup',
+    'arrowdown',
+    'arrowdown',
+    'arrowleft',
+    'arrowright',
+    'arrowleft',
+    'arrowright',
+    'a',
+    'b',
+  ];
+  let keyHistory = [];
+
+  function keyhandler({ key }) {
+    key = key.toLowerCase();
+    keyHistory.push(key);
+    keyHistory = keyHistory.slice(-code.length);
+    if (code.join(',') === keyHistory.join(',')) {
+      load();
+    }
+  }
+
+  document.addEventListener('keyup', keyhandler);
+})();
